4df8d5031d239fb980e2cbbb7acf4037a73acefd
hs697 <43214976+hs697@users.noreply.github.com>
2020-04-20 21:42:21 -0400

    Add files via upload
    Login and registrationSwitch.

--- Login.js
+++ Login.js
@@ -4,42 +4,21 @@ import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import dayjs from "dayjs";
-import { makeStyles } from '@material-ui/core/styles';
 
-const styles = { //style object
-  Button:{
-    margin: 20,
-  }
-}
-/*const styles = makeStyles({ //style object
-    root:{
-      border:0,
-      borderRadius:3,
-      height:48,
-      padding:'0 30px',
-    },
-  });
-const s = styles();
-const useStyles = makeStyles({
-  closeButton: {
-    position: "absolute",
-    right: "8px",
-    top: "8px",
-  },
-});*/
+
 const Login = (props) => (
   
   //<Grid justify = "center" alignItems="center">
   <Grid 
     container 
-    spacing={5}
+    spacing={3}
     direction="column"
     alignItems="center"
     justify="center"
   >
     <Grid item xs={3} />
     <Grid item xs={9}>
-      <Typography variant="h5">Log In Here:</Typography>
+      <Typography variant="h4">Log In Here:</Typography>
     </Grid>
     <Grid item xs={3} />
     <Grid item xs={9}>
@@ -72,7 +51,7 @@ const Login = (props) => (
 
     <Grid item xs={3} />
     <Grid item xs={9}>
-      <Button style = {styles.Button} variant="contained" onClick={props.onLogIn}>
+      <Button variant="contained" onClick={props.onLogIn}>
         Log In
       </Button>
     </Grid>
